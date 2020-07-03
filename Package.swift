1048cef1c3266a2ddb2f05e789b54b3d9735a1a6
Alexandru-Robert <andy_robert97@yahoo.com>
2020-07-03 15:30:58 +0200

    Commit2 - test Xcode

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+//Commit2 comment
 import PackageDescription
 
 let package = Package(
